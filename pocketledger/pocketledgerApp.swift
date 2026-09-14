473550d87db4b94d9e4ddc44646d32dc442c4ec9
Dogukan Cetin <prodoguctn@hotmail.com>
2026-09-14 17:12:01 +0300

    Initial Commit

---
+++ pocketledger/pocketledgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pocketledgerApp.swift
+//  pocketledger
+//
+//  Created by Doğukan Çetin on 14.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct pocketledgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
